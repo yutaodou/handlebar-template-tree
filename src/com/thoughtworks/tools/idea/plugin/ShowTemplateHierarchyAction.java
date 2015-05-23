f1b46bcc30c60add3d8728545235f8d93289c750
Dou Yutao <yutao.dou@rea-group.com>
2015-05-23 15:21:51 +0800

    Show hierarchy template tool window.

--- src/com/thoughtworks/tools/idea/plugin/ShowTemplateHierarchyAction.java
+++ src/com/thoughtworks/tools/idea/plugin/ShowTemplateHierarchyAction.java
@@ -4,11 +4,16 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.thoughtworks.tools.idea.plugin.model.Template;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
 
-import java.util.List;
+import javax.swing.*;
 
 public class ShowTemplateHierarchyAction extends DumbAwareAction {
+
+    public static final String TOOL_WINDOW_ID = "templateHierarchy";
+
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getData(DataKeys.PROJECT);
@@ -17,7 +22,11 @@ public class ShowTemplateHierarchyAction extends DumbAwareAction {
         }
 
         EntryHierarchyBuilder builder = new EntryHierarchyBuilder();
-        List<Template> templates = builder.build(project);
-        System.out.println(templates.size());
+        ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TOOL_WINDOW_ID);
+        if (toolWindow == null) {
+            toolWindow = ToolWindowManager.getInstance(project).registerToolWindow(TOOL_WINDOW_ID, true, ToolWindowAnchor.RIGHT, false);
+            toolWindow.getComponent().add(new JLabel("Show hierarchy here."));
+        }
+        toolWindow.show(null);
     }
 }
